81573eb1f2b1b29aee09c33ba540b5f23ad5d333
psw4887 <psw4887@naver.com>
2023-02-24 13:04:32 +0900

    어플리케이션세팅 완료 (스프링 부트 2.7.3 적용)

---
+++ src/main/java/com/bukibuki/bukibukiclient/BukiBukiClientApplication.java
@@ -0,0 +1,13 @@
+package com.bukibuki.bukibukiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukiBukiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BukiBukiClientApplication.class, args);
+	}
+
+}
